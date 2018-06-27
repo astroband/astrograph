04b3ff4589163f7cd0b25e0919710f51bf4b938c
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-06-27 22:58:59 +0200

    DB setup for dataentry

--- model/main.go
+++ model/main.go
@@ -18,10 +18,12 @@ type AccountThresholds struct {
 }
 type DataEntry struct {
 	ID           string `json:"id"`
-	AccountID    string `json:"accountId"`
-	Name         string `json:"name"`
-	Value        string `json:"value"`
-	LastModified int    `json:"lastModified"`
+	AccountID    string `json:"accountId" db:"accountid"`
+	Name         string `json:"name" db:"dataname"`
+	Value        string `json:"value" db:"-"`
+	LastModified int    `json:"lastModified" db:"lastmodified"`
+
+	RawValue     string `db:"datavalue"`
 }
 type Account struct {
 	ID             string            `json:"id" db:"accountid"`
