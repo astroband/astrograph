c93c498fb7795b0728dc9b8225253614ef34f24e
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-08-14 14:33:38 +0300

    TrustLines repo

---
+++ src/repo/trust_lines.ts
@@ -0,0 +1,20 @@
+import { IDatabase } from "pg-promise";
+import { TrustLine } from "../model";
+
+export default class TrustLinesRepo {
+  private db: IDatabase<any>;
+
+  constructor(db: any) {
+    this.db = db;
+  }
+
+  public async findAllByAccountID(id: string): Promise<TrustLine[]> {
+    const res = await this.db.manyOrNone("SELECT * FROM trustlines WHERE accountid = $1 ORDER BY publickey", id);
+    return res.map(e => new TrustLine(e));
+  }
+
+  public async findAllByAccountIDs(ids: string[]): Promise<TrustLine[][]> {
+    const res = await this.db.manyOrNone("SELECT * FROM trustlines WHERE accountid IN ($1) ORDER BY publickey", ids);
+    return ids.map(id => res.filter(r => r.accountid === id).map(s => new TrustLine(s)));
+  }
+}
