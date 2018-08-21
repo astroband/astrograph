74ff402b8a3c7724ade07b9e9c1f20dd537de935
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-08-21 17:46:49 +0300

    Trashy DGraph initializer

--- src/graphql.ts
+++ src/graphql.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from "apollo-server";
 import startIngest from "./common/util/ingest";
 import logger from "./common/util/logger";
 import schema from "./schema";
+import initDGraph from "./dgraph";
 
 const server = new ApolloServer({
   schema,
@@ -10,6 +11,7 @@ const server = new ApolloServer({
 });
 
 startIngest();
+initDGraph();
 
 server.listen().then(({ url }) => {
   logger.info(`🚀 Server ready at ${url}`);
