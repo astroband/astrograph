7bc0fc18d71b86eaa5c5d1a5ab68991c44605180
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-09-11 17:22:12 +0300

    Updates for balance-monitor-cli

---
+++ examples/balance-monitor-cli.ts
@@ -0,0 +1,64 @@
+// Run: yarn exec ts-node examples/balance-monitor-cli.ts <ACCOUNT ID> [<ASTROGRAPH URL>]
+
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { gql } from "apollo-server";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import ws from "ws";
+
+import { ACCOUNT_ID, GRAPHQL_ENDPOINT } from "./args";
+
+const client = new SubscriptionClient(`ws://${GRAPHQL_ENDPOINT}`, { reconnect: true }, ws);
+
+const link = new WebSocketLink(client);
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({ link, cache });
+
+console.log("Connecting to GraphQL...");
+
+const SUBSCRIPTION = gql`
+  subscription trustLine($args: EventInput!) {
+    account(args: $args) {
+      id
+      values {
+        asset {
+          code
+          native
+        }
+        balance
+        limit
+        authorized
+      }
+    }
+  }
+`;
+
+client.onError(error => console.log("An error occured!", error.target));
+
+apolloClient
+  .subscribe({
+    fetchPolicy: "network-only",
+    query: SUBSCRIPTION,
+    variables: {
+      args: {
+        idEq: ACCOUNT_ID
+      }
+    }
+  })
+  .subscribe({
+    next(data: any) {
+      const values = data.values;
+      console.log(
+        "New balance for",
+        values.asset.code,
+        "is now",
+        values.balance,
+        "with limit ",
+        values.limit,
+        values.authorized ? "and authorization" : "without authorization"
+      );
+    }
+  });
+
+// setTimeout(() => { console.log("Finished.", eventCount, "events received."); }, 10000);
