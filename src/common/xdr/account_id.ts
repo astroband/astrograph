88d034a905c86882ccabeb107a6aeb6f1d23da5c
Timur Ramazanov <charlie.vmk@gmail.com>
2018-09-12 16:48:02 +0300

    Compare signers too

--- src/common/xdr/account_id.ts
+++ src/common/xdr/account_id.ts
@@ -1,4 +1,5 @@
 import stellar from "stellar-base";
+import { signerKeyFromXDR } from ".";
 
 export function publicKeyFromBuffer(value: Buffer): string {
   return stellar.StrKey.encodeEd25519PublicKey(value);
@@ -8,7 +9,6 @@ export function publicKeyFromXDR(xdr: any): string {
   return publicKeyFromBuffer(xdr.accountId().value());
 }
 
-// TODO: compare signers too
 export function diffAccountsXDR(xdr1: any, xdr2: any): string[] {
   const changedAttrs: string[] = [];
 
@@ -35,6 +35,24 @@ export function diffAccountsXDR(xdr1: any, xdr2: any): string[] {
 
   if (!xdr1.thresholds().equals(xdr2.thresholds())) {
     changedAttrs.push("thresholds");
+  }
+
+  if (xdr1.signers().length !== xdr2.signers().length) {
+    changedAttrs.push("signers");
+  } else {
+    const allSignersAreEqual =
+      xdr1
+      .signers()
+      .map((signerXDR: any, i: number) => { return [signerXDR, xdr2.signers()[i]] })
+      .reduce((accumulator: boolean, pair: any[]) => {
+        return accumulator &&
+          signerKeyFromXDR(pair[0].key()) === signerKeyFromXDR(pair[1].key()) &&
+          pair[0].weight() === pair[1].weight()
+      }, true);
+
+    if (!allSignersAreEqual) {
+      changedAttrs.push("signers");
+    }
   }
 
   return changedAttrs;
