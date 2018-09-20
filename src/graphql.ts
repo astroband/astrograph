94a6110318de454100d20a309baf93f721c5aff8
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-09-20 11:46:49 +0400

    Enabled playground on production

--- src/graphql.ts
+++ src/graphql.ts
@@ -11,6 +11,7 @@ const server = new ApolloServer({
   tracing: true,
   mocks: true,
   introspection: true,
+  playground: true,
   debug: process.env.NODE_ENV === "development",
   cors: true
 });
