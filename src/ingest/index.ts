3a3aaf5bf8e90edeb5678a451d93e484e682839a
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-09-29 01:43:17 +0400

    DGraph.ts => storage.ts

--- src/ingest/index.ts
+++ src/ingest/index.ts
@@ -1,3 +1,7 @@
 export { SubscriptionPayloadCollection } from "./subscription_payload_collection";
 export { Cursor } from "./cursor";
+<<<<<<< HEAD
+=======
+export { Storage } from "./storage";
+>>>>>>> DGraph.ts => storage.ts
 export { Worker } from "./worker";
