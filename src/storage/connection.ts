7c06a13e549fb7b2b3dea37343169677d73f1013
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-10-02 17:48:33 +0400

    Connection push

--- src/storage/connection.ts
+++ src/storage/connection.ts
@@ -1,5 +1,6 @@
-import { DgraphClient, DgraphClientStub, Operation } from "dgraph-js";
+import { DgraphClient, DgraphClientStub, Mutation, Operation } from "dgraph-js";
 import grpc from "grpc";
+import logger from "../util/logger";
 import { DGRAPH_URL } from "../util/secrets";
 
 const schema = `
@@ -25,6 +26,23 @@ export class Connection {
     const op = new Operation();
     op.setSchema(schema);
     await this.client.alter(op);
+  }
+
+  public async push(nquads: string): Promise<any> {
+    const txn = this.client.newTxn();
+
+    try {
+      const mu = new Mutation();
+      mu.setSetNquads(nquads);
+      const assigns = await txn.mutate(mu);
+      await txn.commit();
+
+      return assigns;
+    } catch (err) {
+      await txn.discard();
+      logger.error(err);
+      process.exit(-1);
+    }
   }
 }
 
