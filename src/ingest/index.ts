f4d1f4a3a15d66412eaab18472b0d0249dc23ba1
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-10-02 12:58:41 +0400

    Removed unintentional merge commit

--- src/ingest/index.ts
+++ src/ingest/index.ts
@@ -1,7 +1,4 @@
 export { SubscriptionPayloadCollection } from "./subscription_payload_collection";
 export { Cursor } from "./cursor";
-<<<<<<< HEAD
-=======
 export { Storage } from "./storage";
->>>>>>> DGraph.ts => storage.ts
 export { Worker } from "./worker";
