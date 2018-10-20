96896d4e7ff84d395b5afca953749f47c909e3a4
Timur Ramazanov <charlie.vmk@gmail.com>
2018-10-20 12:04:29 +0300

    Set requireResolversForResolveType to false

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,7 +3,12 @@ import { makeExecutableSchema, mergeSchemas } from "apollo-server";
 import resolvers from "./resolvers";
 import typeDefs from "./type_defs";
 
-const schema = makeExecutableSchema({ typeDefs });
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
+});
 const schemas = [schema];
 
 export default mergeSchemas({ schemas, resolvers });
