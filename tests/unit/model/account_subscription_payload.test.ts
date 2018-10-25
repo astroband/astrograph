f6da4a48596b64eda99f84a425c75941d1d7c62d
Timur Ramazanov <charlie.vmk@gmail.com>
2018-10-25 15:47:19 +0300

    AccountSubscriptionPayload model unit tests

---
+++ tests/unit/model/account_subscription_payload.test.ts
@@ -0,0 +1,33 @@
+import { AccountSubscriptionPayload, AccountValues, MutationType } from "../../../src/model";
+import stellar from "stellar-base";
+
+jest.mock("../../../src/model/account_values");
+
+describe("constructor", () => {
+  const rawXDR = "AAAAAQAAAAIAAAADAAAABAAAAAAAAAAAnNaDsWrrYf58AZ/37vPsUpwk0kqEGflGNc9yFOmOyFYAAAAEqBfHnAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAABAAAABAAAAAAAAAAAnNaDsWrrYf58AZ/37vPsUpwk0kqEGflGNc9yFOmOyFYAAAAEqBfHnAAAAAMAAAABAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAABAAAAAwAAAAAAAAAEAAAAAQAAAACc1oOxauth/nwBn/fu8+xSnCTSSoQZ+UY1z3IU6Y7IVgAAAAFLSEwAAAAAAP8RoqirLHHNQiYBsPxJ/OylDilni/4gIJ9wmzoeAiE9AAAAAAAAAAB//////////wAAAAEAAAAAAAAAAAAAAAMAAAAEAAAAAAAAAACc1oOxauth/nwBn/fu8+xSnCTSSoQZ+UY1z3IU6Y7IVgAAAASoF8ecAAAAAwAAAAEAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAEAAAAEAAAAAAAAAACc1oOxauth/nwBn/fu8+xSnCTSSoQZ+UY1z3IU6Y7IVgAAAASoF8ecAAAAAwAAAAEAAAABAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAA==";
+  const txMeta = stellar.xdr.TransactionMeta.fromXDR(Buffer.from(rawXDR, "base64"));
+  const xdr = txMeta.v1().operations()[0].changes()[2].updated().data().account();
+
+  let subject: AccountSubscriptionPayload;
+
+  it("sets account id and mutation type", () => {
+    subject = new AccountSubscriptionPayload(MutationType.Update, xdr);
+
+    expect(subject.id).toBe("GCONNA5RNLVWD7T4AGP7P3XT5RJJYJGSJKCBT6KGGXHXEFHJR3EFNLTE");
+    expect(subject.mutationType).toBe(MutationType.Update);
+  });
+
+  describe("mutation type is 'Remove'", () => {
+    it("doesn't set values", () => {
+      subject = new AccountSubscriptionPayload(MutationType.Remove, xdr);
+      expect(subject.values).toBeNull()
+    });
+  });
+
+  describe("mutation type is not 'Remove'", () => {
+    it("build values from XDR", () => {
+      subject = new AccountSubscriptionPayload(MutationType.Update, xdr);
+      expect(AccountValues.buildFromXDR).toHaveBeenCalledWith(xdr);
+    });
+  });
+});
