871d401ed6d202d9e9df2d392a8be02f139d39a2
Timur Ramazanov <charlie.vmk@gmail.com>
2018-11-08 18:14:28 +0300

    Fix buffer memo values ingesting

--- src/graphql.ts
+++ src/graphql.ts
@@ -6,6 +6,7 @@ import { Connection } from "./storage";
 import logger from "./util/logger";
 import { BIND_ADDRESS, DEBUG_LEDGER, DGRAPH_URL, INGEST_INTERVAL, PORT } from "./util/secrets";
 import { setNetwork as setStellarNetwork } from "./util/stellar";
+import "./util/memo";
 
 const isDev = process.env.NODE_ENV === "development";
 
--- src/schema/resolvers/util.ts
+++ src/schema/resolvers/util.ts
@@ -1,4 +1,4 @@
-import { Asset, Memo, MemoText, MemoHash, MemoReturn } from "stellar-sdk";
+import { Asset, Memo } from "stellar-sdk";
 import { createBatchResolver as create } from "graphql-resolve-batch";
 import { Ledger, MutationType } from "../../model";
 
@@ -12,24 +12,15 @@ export function ledgerResolver(obj: any) {
 }
 
 export function memoResolver(obj: any) {
-  const memo = obj.memo as Memo;
-  let value = memo.value;
-
-  if (value instanceof Buffer) {
-    switch(memo.type) {
-      case MemoText:
-        value = value.toString("utf8");
-        break;
-      case MemoHash:
-      case MemoReturn:
-        value = value.toString("base64");
-        break;
-    }
+  if (!obj.memo) {
+    return null;
   }
+
+  const memo = obj.memo as Memo;
 
   return {
     type: memo.type,
-    value: value
+    value: memo.getPlainValue()
   }
 }
 
