91f0e05648bb889da48112638a82f42f1326ec6d
Timur Ramazanov <charlie.vmk@gmail.com>
2018-11-22 16:38:15 +0300

    Change accountMerge op data structure

--- src/storage/builders/operations/account_merge_op.ts
+++ src/storage/builders/operations/account_merge_op.ts
@@ -1,5 +1,5 @@
 import { publicKeyFromBuffer } from "../../../util/xdr/account";
-import { NQuads } from "../../nquads";
+import { NQuad, NQuads } from "../../nquads";
 import { AccountBuilder } from "../account";
 import { SpecificOperationBuilder } from "../specific_operation";
 
@@ -17,9 +17,19 @@ export class AccountMergeOpBuilder extends SpecificOperationBuilder {
   }
 
   protected pushResult() {
-    this.pushValue(
-      "source_account_balance",
-      this.trXDR.accountMergeResult().sourceAccountBalance().toString()
+    const result = this.trXDR.accountMergeResult();
+    this.pushValue("account_merge_result_code", result.switch().value);
+
+    const resultNQuad = NQuad.blank(`${this.current.value}_result`);
+
+    this.nquads.push(
+      new NQuad(
+        resultNQuad,
+        "source_account_balance",
+        NQuad.value(result.sourceAccountBalance().toString())
+      )
     );
+
+    this.nquads.push(new NQuad(this.current, "result", resultNQuad));
   }
 }
