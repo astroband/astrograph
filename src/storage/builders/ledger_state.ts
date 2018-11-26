d2209e1cce35530ec3c58fa24f92043927ef73e4
Timur Ramazanov <charlie.vmk@gmail.com>
2018-11-26 18:09:02 +0300

    Trustline changes ingestion

--- src/storage/builders/ledger_state.ts
+++ src/storage/builders/ledger_state.ts
@@ -1,6 +1,8 @@
 import stellar from "stellar-base";
 import { TrustLineValues } from "../../model/trust_line_values";
+import { Connection } from "../connection";
 import { NQuads } from "../nquads";
+import { LastTrustLineEntryQuery } from "../queries/last_trust_line_entry";
 import { Builder } from "./builder";
 import { TrustLineEntryBuilder } from "./trust_line_entry";
 
@@ -12,11 +14,14 @@ export class LedgerStateBuilder {
 
   constructor(private changes: any[]) {}
 
-  public build(): NQuads {
+  public async build(): Promise<NQuads> {
     for (const change of this.changes) {
       switch (change.switch()) {
         case changeType.ledgerEntryCreated():
           this.pushCreated(change.created());
+          break;
+        case changeType.ledgerEntryUpdated():
+          await this.pushUpdated(change.updated());
           break;
       }
     }
@@ -32,6 +37,25 @@ export class LedgerStateBuilder {
         const data = TrustLineValues.buildFromXDR(xdr.data().trustLine());
         data.lastModified = xdr.lastModifiedLedgerSeq();
         builder = new TrustLineEntryBuilder(data);
+
+        this.nquads.push(...builder.build());
+        break;
+    }
+  }
+
+  private async pushUpdated(xdr: any) {
+    let builder: Builder;
+
+    const c = new Connection();
+
+    switch (xdr.data().switch()) {
+      case ledgerEntryType.trustline():
+        const data = TrustLineValues.buildFromXDR(xdr.data().trustLine());
+        const prevQuery = new LastTrustLineEntryQuery(c, data.asset, data.accountID);
+
+        const prev = await prevQuery.call();
+        data.lastModified = xdr.lastModifiedLedgerSeq();
+        builder = new TrustLineEntryBuilder(data, prev as string);
 
         this.nquads.push(...builder.build());
         break;
--- src/storage/builders/trust_line_entry.ts
+++ src/storage/builders/trust_line_entry.ts
@@ -18,17 +18,23 @@ export class TrustLineEntryBuilder extends Builder {
 
   public readonly current: IBlank;
 
-  constructor(private trustLine: ITrustLine) {
+  constructor(private trustLine: ITrustLine, prevKey?: string) {
     super();
     this.current = NQuad.blank(TrustLineEntryBuilder.key(trustLine));
+
+    if (prevKey) {
+      this.prev = NQuad.blank(prevKey);
+    }
   }
 
   public build(): NQuads {
+    this.pushKey();
     this.pushBuilder(new AccountBuilder(this.trustLine.accountID), "account");
     this.pushBuilder(new AssetBuilder(this.trustLine.asset), "asset");
     this.pushValue("balance", this.trustLine.balance);
     this.pushValue("ledger_seq", this.trustLine.lastModified);
     this.pushValue("type", "trust_line_entry");
+    this.pushPrev();
     return this.nquads;
   }
 }
