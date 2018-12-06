21bf9ce27667238d0b3e582390032f351605900c
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-12-06 16:29:34 +0400

    Asset with builder extends Asset

--- src/util/asset.ts
+++ src/util/asset.ts
@@ -1,7 +1,7 @@
 import stellar from "stellar-base";
 import { Asset } from "stellar-sdk";
 
-export default class {
+export default class extends Asset {
   public static build(type: number, code: string, issuer: string): Asset {
     return type === stellar.xdr.AssetType.assetTypeNative().value ? Asset.native() : new Asset(code, issuer);
   }
