b880fb2c2795840a6290047c3be735a6ed3867b0
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-01-10 13:49:14 +0400

    Removed obsolete comment

--- src/model2/index.ts
+++ src/model2/index.ts
@@ -1,15 +1,3 @@
-// Models represent GraphQL types from schema.
-//
-// Conventions:
-//
-// 1. Models must not contain relations: we load related via resolvers (ex: Transactions have no an array of
-//    operations).
-// 2. Models must have sufficient data to load related from either database or DGraph (ex: Account has
-//    inflationDest as scalar, not as an Account).
-// 3. Models have static factory methods with "from*" signature used as constructors (ex: fromValue,
-//    fromXdr, fromDb, ...).
-// 4. Some of types in model ns might not be reflected in GraphQL yet.
-//
 export * from "./account";
 export * from "./account_thresholds";
 export * from "./account_flags";
