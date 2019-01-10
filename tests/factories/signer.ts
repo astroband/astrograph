bd5ba5c2ffb734c5484754f39e8a4475156fae00
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-01-10 17:35:31 +0400

    Signer spec factory taken from model2 now

--- tests/factories/signer.ts
+++ tests/factories/signer.ts
@@ -1,11 +1,11 @@
 import stellar from "stellar-base";
 import { Factory } from "rosie";
-import { Signer } from "../../src/model/signer";
+import { Signer } from "../../src/model2/signer";
 
 Factory.define("signer")
-  .attr("accountid", () => { return stellar.Keypair.random().publicKey() })
-  .attr("publickey", () => { return stellar.Keypair.random().publicKey() })
-  .attr("weight", () => { return Math.floor(Math.random() * 5) });
+  .attr("accountID", () => stellar.Keypair.random().publicKey())
+  .attr("signer", () => stellar.Keypair.random().publicKey())
+  .attr("weight", () => Math.floor(Math.random() * 5));
 
 export default {
   build(overrides?: object): Signer {
