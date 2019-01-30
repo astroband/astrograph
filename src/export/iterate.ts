e2a3a489aaf5116b41794f90b6bfaae1468b11ec
Timur Ramazanov <charlie.vmk@gmail.com>
2019-01-30 13:58:45 +0300

    Log average execution time in export

--- src/export/iterate.ts
+++ src/export/iterate.ts
@@ -1,7 +1,21 @@
+import _ from "lodash";
+import logger from "../util/logger";
 import { db } from "../database";
 
 export default async function iterate(min: number, max: number, cb: any) {
+  let totalTimes = [];
+  let ingestTimes = [];
+
   for (let n = min; n <= max; n++) {
+    const totalStart = _.now();
+
+    if (n % 1000 === 0) {
+      logger.debug(`Average total time for last 1000 ledgers: ${_.sum(totalTimes) / totalTimes.length}`);
+      logger.debug(`Average ingest time for last 1000 ledgers: ${_.sum(ingestTimes) / ingestTimes.length}`);
+      totalTimes = [];
+      ingestTimes = [];
+    } 
+
     let header = await db.ledgerHeaders.findBySeq(n);
 
     if (!header) {
@@ -16,6 +30,10 @@ export default async function iterate(min: number, max: number, cb: any) {
 
     const transactions = await db.transactions.findAllBySeq(header.ledgerSeq);
 
+    const ingestStart = _.now();
     await cb(header, transactions);
+
+    ingestTimes.push(_.now() - ingestStart);
+    totalTimes.push(_.now() - totalStart);
   }
 }
