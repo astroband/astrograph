8bf0c30df4da0ce4ef4d1533f067e3e050430f7f
Timur Ramazanov <charlie.vmk@gmail.com>
2019-02-06 16:07:22 +0300

    Add comments to schema, remove system predicates

--- src/storage/schema.ts
+++ src/storage/schema.ts
@@ -1,52 +1,17 @@
 // Unfortunately, DGraph schema doesn't support comments,
 // so we'll remove them later before exporting, using regex
 const schemaWithComments = `
-  _ingested: default .
-  account: uid .
-  account.destination: uid .
-  account.inflation_dest: uid .
-  account.source: uid .
-  account_merge_result_code: default .
-  allow_trust_result_code: default .
-  amount: int @index(int) .
-  amount_bought: default .
-  amount_sold: default .
-  asset: uid .
-  asset.bought: uid .
-  asset.buying: uid .
-  asset.destination: uid .
-  asset.id: default .
-  asset.selling: uid .
-  asset.sold: uid .
-  asset.source: uid .
-  asset_code: string @index(exact) .
-  assets.issued: uid .
-  assets.path: uid .
-  authorize: bool @index(bool) .
-  balance: default .
-  base_fee: default .
-  base_reserve: default .
-  bump_to: int @index(int) .
-  change_trust_result_code: default .
-  clear_flags: default .
-  close_time: datetime @index(hour) .
-  code: string @index(exact) .
-  create_account_result_code: default .
-  deleted: bool @index(bool) .
-  dest_amount: default .
-  dgraph.group.acl: string .
-  dgraph.password: password .
-  dgraph.user.group: uid .
-  dgraph.xid: string @index(exact) .
-  fee_amount: default .
-  fee_charged: default .
-  high: default .
-  home_domain: default .
-  id: string @index(exact) .
-  index: int @index(int) .
-  issuer: uid .
+  # common
+  order: int @index(int) .
   key: string @index(hash) .
-  kind: string @index(hash) .
+  id: string @index(exact) .
+  type: string @index(hash) .
+  type.account: default .
+  type.asset: default .
+  type.ledger: default .
+  type.operation: default .
+  type.transaction: default .
+  type.trust_line_entry: default .
   kind.accountMerge: default .
   kind.allowTrust: default .
   kind.changeTrust: default .
@@ -58,56 +23,130 @@ const schemaWithComments = `
   kind.pathPayment: default .
   kind.payment: default .
   kind.setOption: default .
-  last: uid .
+  index: int @index(int) .
+  account.source: uid .
   ledger: uid .
-  limit: int @index(int) .
-  low: default .
-  manage_data_result_code: default .
-  master_weight: default .
+  next: uid .
+  prev: uid .
+  asset: uid .
+
+  # ledgers
+  seq: int @index(int) .
+  version: default .
+  base_fee: default .
+  base_reserve: default .
   max_tx_set_size: default .
-  med: default .
+  close_time: datetime @index(hour) .
+  _ingested: default .
+
+  # accounts
+  deleted: bool @index(bool) .
+  assets.issued: uid .
+  operations: uid .
+  transactions: uid .
+
+  # assets
+  asset.id: default .
+  issuer: uid .
+  code: string @index(exact) .
+  native: bool @index(bool) .
+
+  # transactions
+  result_code: default .
+  fee_amount: default .
+  fee_charged: default .
+  success: default .
+  time_bounds.min: default .
+  time_bounds.max: default .
   memo.type: default .
   memo.value: default .
-  name: string @index(exact) .
-  native: bool @index(bool) .
-  next: uid .
-  offer_id: string @index(exact) .
-  offers: uid .
-  operations: uid .
-  order: int @index(int) .
-  path_payment_result_code: default .
-  payment_result_code: default .
-  prev: uid .
-  price: float @index(float) .
-  price_d: default .
-  price_n: default .
-  result: uid .
-  result_code: default .
-  seller: uid .
-  send_max: default .
-  seq: int @index(int) .
-  set_flags: default .
-  set_options_result_code: default .
-  signer: uid .
-  source_account_balance: default .
-  starting_balance: int @index(int) .
-  success: default .
-  thresholds: uid .
-  time_bounds.max: default .
-  time_bounds.min: default .
+
+  # trustline entry
+  balance: default .
+
+  # operations
   transaction: uid .
-  transactions: uid .
+  kind: string @index(hash) .
+  amount: int @index(int) .
+  account.destination: uid .
+  result: uid .
+
+  # account merge
+  account_merge_result_code: default .
+
+  # allow trust
   trustor: uid .
-  type: string @index(hash) .
-  type.account: default .
-  type.asset: default .
-  type.ledger: default .
-  type.operation: default .
-  type.transaction: default .
-  type.trust_line_entry: default .
+  asset_code: string @index(exact) .
+  authorize: bool @index(bool) .
+  allow_trust_result_code: default .
+
+  # bump sequence
+  bump_to: int @index(int) .
+
+  # change trust
+  limit: int @index(int) .
+  change_trust_result_code: default .
+
+  # create account
+  starting_balance: int @index(int) .
+  create_account_result_code: default .
+
+  # manage data
+  name: string @index(exact) .
   value: string @index(exact) .
-  version: default .
+  manage_data_result_code: default .
+
+  # manage offer
+  asset.buying: uid .
+  asset.selling: uid .
+  price_n: default .
+  price_d: default .
+  price: float @index(float) .
+  offer_id: string @index(exact) .
+
+  # payment
+  payment_result_code: default .
+
+  # path payment
+  asset.source: uid .
+  asset.destination: uid .
+  assets.path: uid .
+  send_max: default .
+  dest_amount: default .
+  path_payment_result_code: default .
+
+  # set options
+  signer: uid .
+  account.inflation_dest: uid .
+  clear_flags: default .
+  set_flags: default .
+  master_weight: default .
+  home_domain: default .
+  thresholds: uid .
+  set_options_result_code: default .
+
+  # thresholds
+  low: default .
+  med: default .
+  high: default .
+
+  # signer
+  account: uid .
   weight: default .
+
+  # operation results
+  # account merge result
+  source_account_balance: int .
+
+  # path payment result
+  no_issuer: uid .
+  last: uid .
+  offers: uid .
+  asset.sold: uid .
+  asset.bought: uid .
+  amount_sold: default .
+  amount_bought: default .
+  seller: uid .
 `;
 
 // remove comments
