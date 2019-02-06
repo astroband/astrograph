c42f8c7f03ffdb8bd3162b5bc4e6b46ceecc4a02
Timur Ramazanov <charlie.vmk@gmail.com>
2019-02-06 15:19:05 +0300

    Full schema from working DGraph node

--- src/storage/schema.ts
+++ src/storage/schema.ts
@@ -1,134 +1,113 @@
 // Unfortunately, DGraph schema doesn't support comments,
 // so we'll remove them later before exporting, using regex
 const schemaWithComments = `
-  # common
-  order: int @index(int) .
-  key: string @index(hash) .
-
-  # ledgers
-  ledger.id: int @index(int) .
-  version: int .
-  base_fee: int .
-  base_reserve: int .
-  max_tx_set_size: int .
-  close_time: dateTime @index(hour) .
-
-  # accounts
-  account.id: string @index(exact) .
-  deleted: bool @index(bool) .
-
-  # assets
-  asset.id: string @index(exact) .
-  asset.issuer: uid @reverse .
-  code: string @index(exact) .
-  native: bool @index(bool) .
-
-  # transactions
-  tx.id: string @index(exact) .
-  tx.ledger: uid @reverse .
-  tx.source: uid @reverse .
-  tx.index: int .
-  tx.next: uid @reverse .
-  result_code: int .
-  fee_amount: int .
-  fee_charged: int .
-  success: bool @index(bool) .
-  time_bounds.min: dateTime .
-  time_bounds.max: dateTime .
-  memo.type: string .
-  memo.value: string .
-
-  # operations
-  op.id: string @index(exact) .
-  op.transaction: uid @reverse .
-  op.index: int .
-  op.source: uid @reverse .
-  op.destination: uid @reverse .
-  op.kind: string @index(hash) .
-  amount: int .
-
-  # account merge
-  result: uid .
-  account_merge_result_code: int .
-
-  # allow trust
-  allow_trust_op.trustor: uid @reverse .
-  asset_code: string @index(exact) .
-  authorize: bool @index(bool) .
-  allow_trust_result_code: int .
-
-  # bump sequence
-  bump_to: int @index(int) .
-  bump_sequence_result_code: int .
-
-  # change trust
-  change_trust_op.asset: uid @reverse .
-  limit: int @index(int) .
-  change_trust_result_code: int .
-
-  # create account
-  starting_balance: int @index(int) .
-  create_account_result_code: int .
-
-  # manage data
-  name: string @index(exact) .
-  value: string @index(exact) . 
-  manage_data_result_code: int .
-
-  # manage offer
-  manage_offer_op.asset_buying: uid @reverse .
-  manage_offer_op.asset_selling: uid @reverse .
-  price_n: int .
-  price_d: int .
-  price: float .
-  offer_id: string @index (exact) .
-
-  # payment
-  payment_op.asset: uid @reverse .
-  payment_result_code: int .
-
-  # path payment
-  path_payment_op.asset_source: uid @reverse .
-  path_payment_op.asset_destination: uid @reverse .
-  path_payment_op.assets_path: uid @reverse .
-  send_max: int .
-  dest_amount: int .
-  
-  # set options
-  set_options_op.signer: uid @reverse .
-  set_options_op.inflation_destination: uid .
-  clear_flags: int .
-  set_flags: int .
-  master_weight: int .
-  signer: uid .
-  home_domain: string .
-  thresholds: uid .
-  set_options_result_code: int .
-
-  # thresholds
-  high: int .
-  med: int .
-  low: int .
-
-  # signer
+  _ingested: default .
   account: uid .
-  weight: int .
-
-  # operation results
-  # account merge result
-  source_account_balance: int .
-
-  # path payment result
-  path_payment_result.no_issuer: uid .
-  path_payment_result.last: uid .
-  destination: uid .
-  last.asset: uid .
-  path_payment_result.offers: uid .
-  asset_sold: uid .
-  asset_bought: uid .
-  amount_sold: int .
-  amount_bought: int .
+  account.destination: uid .
+  account.inflation_dest: uid .
+  account.source: uid .
+  account_merge_result_code: default .
+  allow_trust_result_code: default .
+  amount: int @index(int) .
+  amount_bought: default .
+  amount_sold: default .
+  asset: uid .
+  asset.bought: uid .
+  asset.buying: uid .
+  asset.destination: uid .
+  asset.id: default .
+  asset.selling: uid .
+  asset.sold: uid .
+  asset.source: uid .
+  asset_code: string @index(exact) .
+  assets.issued: uid .
+  assets.path: uid .
+  authorize: bool @index(bool) .
+  balance: default .
+  base_fee: default .
+  base_reserve: default .
+  bump_to: int @index(int) .
+  change_trust_result_code: default .
+  clear_flags: default .
+  close_time: datetime @index(hour) .
+  code: string @index(exact) .
+  create_account_result_code: default .
+  deleted: bool @index(bool) .
+  dest_amount: default .
+  dgraph.group.acl: string .
+  dgraph.password: password .
+  dgraph.user.group: uid .
+  dgraph.xid: string @index(exact) .
+  fee_amount: default .
+  fee_charged: default .
+  high: default .
+  home_domain: default .
+  id: string @index(exact) .
+  index: int @index(int) .
+  issuer: uid .
+  key: string @index(hash) .
+  kind: string @index(hash) .
+  kind.accountMerge: default .
+  kind.allowTrust: default .
+  kind.changeTrust: default .
+  kind.createAccount: default .
+  kind.createPassiveOffer: default .
+  kind.inflation: default .
+  kind.manageDatum: default .
+  kind.manageOffer: default .
+  kind.pathPayment: default .
+  kind.payment: default .
+  kind.setOption: default .
+  last: uid .
+  ledger: uid .
+  limit: int @index(int) .
+  low: default .
+  manage_data_result_code: default .
+  master_weight: default .
+  max_tx_set_size: default .
+  med: default .
+  memo.type: default .
+  memo.value: default .
+  name: string @index(exact) .
+  native: bool @index(bool) .
+  next: uid .
+  offer_id: string @index(exact) .
+  offers: uid .
+  operations: uid .
+  order: int @index(int) .
+  path_payment_result_code: default .
+  payment_result_code: default .
+  prev: uid .
+  price: float @index(float) .
+  price_d: default .
+  price_n: default .
+  result: uid .
+  result_code: default .
   seller: uid .
+  send_max: default .
+  seq: int @index(int) .
+  set_flags: default .
+  set_options_result_code: default .
+  signer: uid .
+  source_account_balance: default .
+  starting_balance: int @index(int) .
+  success: default .
+  thresholds: uid .
+  time_bounds.max: default .
+  time_bounds.min: default .
+  transaction: uid .
+  transactions: uid .
+  trustor: uid .
+  type: string @index(hash) .
+  type.account: default .
+  type.asset: default .
+  type.ledger: default .
+  type.operation: default .
+  type.transaction: default .
+  type.trust_line_entry: default .
+  value: string @index(exact) .
+  version: default .
+  weight: default .
 `;
 
 // remove comments
