fe85f777af21591fd1e4ed7859697ff96d350b85
Sergey Nebolsin <sergey@nebols.in>
2019-02-12 13:12:09 +0300

    Cleanup project configuration

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  coverageReporters: ["text", "text-summary", "json"],
-  coverageDirectory: "coverage",
-  roots: ["<rootDir>/tests/"],
-};
