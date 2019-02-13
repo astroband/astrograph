a916b090bc813ec9f1843a0530ba509f5b1ef698
Timur Ramazanov <charlie.vmk@gmail.com>
2019-02-13 08:21:25 +0300

    Slice exported RDF files

--- src/export.ts
+++ src/export.ts
@@ -1,4 +1,6 @@
+import fs from "fs";
 import ProgressBar from "progress";
+import zlib  from "zlib";
 import { Ingestor } from "./storage/ingestor";
 
 import { configure, IConfig } from "./export/configure";
@@ -9,16 +11,46 @@ import { LedgerHeader, TransactionWithXDR } from "./model";
 
 logger.info("DGraph live export script, yarn export <count> <seq>");
 
-configure().then(async (config: IConfig) => {
-  const bar = new ProgressBar("[:bar] :elapseds elapsed, eta :etas", { total: config.total });
+configure()
+  .then(async (config: IConfig) => {
+    const bar = new ProgressBar("[:bar] :elapseds elapsed, eta :etas", { total: config.total });
 
-  logger.info(`Exporting ${config.total} ledgers ${config.minSeq}/${config.maxSeq}`);
+    logger.info(`Exporting ${config.total} ledgers ${config.minSeq}/${config.maxSeq}`);
 
-  await iterate(config.minSeq, config.maxSeq, async (header: LedgerHeader, transactions: TransactionWithXDR[]) => {
-    const chunk = await Ingestor.ingestLedger(header, transactions);
-    config.file.write(chunk.toString() + "\n");
-    bar.tick();
+    const buildSliceFileName = (sliceNum: number) => {
+      const { minSeq, maxSeq, sliceSize } = config;
+      const sliceStart = minSeq + sliceNum * sliceSize;
+      let sliceEnd = minSeq + (sliceNum + 1) * sliceSize - 1;
+
+      if (sliceEnd > maxSeq) {
+        sliceEnd = maxSeq;
+      }
+
+      return `${config.dirPath}/${sliceStart}-${sliceEnd}.rdf.gz`;
+    };
+
+    let ledgerCounter = 0;
+    let sliceCounter = -1;
+    let file = zlib.createGzip();
+
+    await iterate(config.minSeq, config.maxSeq, async (header: LedgerHeader, transactions: TransactionWithXDR[]) => {
+      if (ledgerCounter % config.sliceSize === 0) {
+        file.end();
+        file = zlib.createGzip();
+
+        sliceCounter += 1;
+        file.pipe(fs.createWriteStream(buildSliceFileName(sliceCounter)));
+      }
+
+      const chunk = await Ingestor.ingestLedger(header, transactions);
+      file.write(chunk.toString() + "\n");
+
+      ledgerCounter += 1;
+      bar.tick();
+    });
+
+    file.end();
+  })
+  .catch(err => {
+    logger.error(err);
   });
-
-  config.file.end();
-});
