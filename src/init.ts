a9bcc2a489563d6621e6adfe68d03449cc60215e
Timur Ramazanov <charlie.vmk@gmail.com>
2019-02-14 17:17:54 +0300

    Explicit init call

--- src/init.ts
+++ src/init.ts
@@ -3,12 +3,14 @@ import logger from "./util/logger";
 import { SENTRY_DSN } from "./util/secrets";
 import { setNetwork as setStellarNetwork } from "./util/stellar";
 
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    integrations: [new Sentry.Integrations.RewriteFrames({ root: __dirname || process.cwd() })]
-  });
-}
+export default function init(): void {
+  if (SENTRY_DSN) {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      integrations: [new Sentry.Integrations.RewriteFrames({ root: __dirname || process.cwd() })]
+    });
+  }
 
-const network = setStellarNetwork();
-logger.info(`Using ${network}`);
+  const network = setStellarNetwork();
+  logger.info(`Using ${network}`);
+}
