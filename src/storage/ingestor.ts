bb933d1e5140b94c14447dae6162ae956579af52
Timur Ramazanov <charlie.vmk@gmail.com>
2019-02-27 11:15:35 +0300

    :cop:

--- src/storage/ingestor.ts
+++ src/storage/ingestor.ts
@@ -9,7 +9,7 @@ const defaultOpts: IIngestOpts = { ingestOffers: false };
 
 export class Ingestor {
   public static async ingestLedger(
-    header: LedgerHeader, //@ts-ignore
+    header: LedgerHeader, // @ts-ignore
     transactions: TransactionWithXDR[],
     opts: IIngestOpts = defaultOpts
   ) {
