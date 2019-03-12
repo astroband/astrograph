0ba4bfd0109d8ad38c998d25efe96cbf2bcb0d68
Timur Ramazanov <charlie.vmk@gmail.com>
2019-03-12 16:21:32 +0300

    [GraphQL] Set options operation subscription

--- src/util/xdr_refiner.ts
+++ src/util/xdr_refiner.ts
@@ -1,42 +1,44 @@
 import BigNumber from "bignumber.js";
 import stellar from "stellar-base";
 import { publicKeyFromBuffer } from "./xdr/account";
+import { signerKeyFromXDR } from "./xdr/signer";
 
 export function refineOperationXDR(xdr: any) {
   const t = stellar.xdr.OperationType;
   const source = xdr.sourceAccount() ? publicKeyFromBuffer(xdr.sourceAccount().value()) : null;
   let obj: any;
+  const body = xdr.body();
 
   switch (xdr.body().switch()) {
     case t.createAccount():
-      obj = refineCreateAccountOpXDR(xdr.body().createAccountOp());
+      obj = refineCreateAccountOpXDR(body.createAccountOp());
       break;
     case t.pathPayment():
-      obj = refinePathPaymentOpXDR(xdr.body().pathPaymentOp());
+      obj = refinePathPaymentOpXDR(body.pathPaymentOp());
       break;
     case t.setOption():
-      obj = refineSetOptionOpXDR(xdr.body().setOptionsOp());
+      obj = refineSetOptionsOpXDR(body.setOptionsOp());
       break;
     case t.changeTrust():
-      obj = refineChangeTrustOpXDR(xdr.body().changeTrustOp());
+      obj = refineChangeTrustOpXDR(body.changeTrustOp());
       break;
     case t.accountMerge():
-      obj = refineAccountMergeOpXDR(xdr.body());
+      obj = refineAccountMergeOpXDR(body);
       break;
     case t.manageDatum():
-      obj = refineManageDataOpXDR(xdr.body().manageDataOp());
+      obj = refineManageDataOpXDR(body.manageDataOp());
       break;
     case t.allowTrust():
-      obj = refineAllowTrustOpXDR(xdr.body().allowTrustOp());
+      obj = refineAllowTrustOpXDR(body.allowTrustOp());
       break;
     case t.bumpSequence():
-      obj = refineBumpSequenceOpXDR(xdr.body().bumpSequenceOp());
+      obj = refineBumpSequenceOpXDR(body.bumpSequenceOp());
       break;
     case t.manageOffer():
-      obj = refineManageOfferOpXDR(xdr.body().manageOfferOp());
+      obj = refineManageOfferOpXDR(body.manageOfferOp());
       break;
     case t.payment():
-      obj = refinePaymentOpXDR(xdr.body().paymentOp());
+      obj = refinePaymentOpXDR(body.paymentOp());
       break;
     default:
       throw new Error("Unknown operation XDR");
@@ -44,7 +46,7 @@ export function refineOperationXDR(xdr: any) {
 
   return {
     source,
-    kind: xdr.body().switch().name,
+    kind: body.switch().name,
     ...obj
   };
 }
@@ -78,8 +80,29 @@ function refineCreateAccountOpXDR(body: any) {
 function refinePathPaymentOpXDR(body: any) {
   return {};
 }
-function refineSetOptionOpXDR(body: any) {
-  return {};
+
+function refineSetOptionsOpXDR(body: any) {
+  const options: any = {
+    masterWeight: body.masterWeight(),
+    homeDomain: body.homeDomain().toString(),
+    clearFlags: body.clearFlags(),
+    setFlags: body.setFlags(),
+    thresholds: {
+      high: body.highThreshold(),
+      medium: body.medThreshold(),
+      low: body.lowThreshold()
+    },
+    inflationDestination: publicKeyFromBuffer(body.inflationDest().value())
+  };
+
+  if (body.signer()) {
+    options.signer = {
+      account: signerKeyFromXDR(body.signer().key()),
+      weight: body.signer().weight()
+    };
+  }
+
+  return options;
 }
 
 function refineChangeTrustOpXDR(body: any) {
@@ -106,7 +129,11 @@ function refineAllowTrustOpXDR(body: any) {
   return {
     trustor: publicKeyFromBuffer(body.trustor().value()),
     authorize: body.authorize(),
-    asset: body.asset().value().toString().replace(/\0/g, "")
+    asset: body
+      .asset()
+      .value()
+      .toString()
+      .replace(/\0/g, "")
   };
 }
 
