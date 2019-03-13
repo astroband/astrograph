504edef04b099d3e59c1df2e3bb5c9d04057a6f9
Timur Ramazanov <charlie.vmk@gmail.com>
2019-03-13 20:17:36 +0300

    Cleanup

--- src/ingest/worker.ts
+++ src/ingest/worker.ts
@@ -1,9 +1,9 @@
 import { Publisher } from "../pubsub";
 import { Cursor } from "./cursor";
 
-// import { LedgerStateParser } from "../ledger_state_parser";
-// import { Connection } from "../storage/connection";
-// import { DGRAPH_INGEST_URL } from "../util/secrets";
+import { LedgerStateParser } from "../ledger_state_parser";
+import { Connection } from "../storage/connection";
+import { DGRAPH_INGEST_URL } from "../util/secrets";
 
 export class Worker {
   public cursor: Cursor;
@@ -20,14 +20,14 @@ export class Worker {
 
       await Publisher.publish(header, transactions);
 
-      // if (DGRAPH_INGEST_URL) {
-      //   const c = new Connection(DGRAPH_INGEST_URL);
-      //   const stateParser = new LedgerStateParser(transactions);
-      //   stateParser.parse();
-      //   await c.importLedger(header, transactions, { ingestOffers: true });
-      //   await c.deleteOffers(stateParser.deletedOfferIds);
-      //   c.close();
-      // }
+      if (DGRAPH_INGEST_URL) {
+        const c = new Connection(DGRAPH_INGEST_URL);
+        const stateParser = new LedgerStateParser(transactions);
+        stateParser.parse();
+        await c.importLedger(header, transactions, { ingestOffers: true });
+        await c.deleteOffers(stateParser.deletedOfferIds);
+        c.close();
+      }
 
       return true;
     }
