94a61ec42aee713cd3a806e7ee6d0982c111cc23
Timur Ramazanov <charlie.vmk@gmail.com>
2019-03-15 15:20:48 +0300

    Replace sorting with reverse

--- src/schema/resolvers/transaction.ts
+++ src/schema/resolvers/transaction.ts
@@ -1,5 +1,4 @@
 import { IHorizonTransactionData } from "../../datasource/types";
-import { Transaction } from "../../model";
 import { TransactionWithXDRFactory } from "../../model/factories";
 import { ledgerResolver, memoResolver } from "./util";
 
@@ -15,29 +14,25 @@ export default {
     },
     async transactions(root: any, args: any, ctx: any, info: any) {
       const { first, last, after, before } = args;
-      const records = await ctx.dataSources.horizon.getTransactions(
+      let records = await ctx.dataSources.horizon.getTransactions(
         first || last,
         last ? "asc" : "desc",
         last ? before : after
       );
 
-      return {
-        edges: records
-          .map((record: IHorizonTransactionData) => {
-            return {
-              node: TransactionWithXDRFactory.fromHorizon(record),
-              cursor: record.paging_token
-            };
-          })
-          .sort((a: { node: Transaction }, b: { node: Transaction }) => {
-            // we must keep descending ordering, because Horizon doesn't do it,
-            // when you request the previous page
-            if (b.node.ledgerSeq !== a.node.ledgerSeq) {
-              return b.node.ledgerSeq - a.node.ledgerSeq;
-            }
+      // we must keep descending ordering, because Horizon doesn't do it,
+      // when you request the previous page
+      if (last) {
+        records = records.reverse();
+      }
 
-            return b.node.index - a.node.index;
-          }),
+      return {
+        edges: records.map((record: IHorizonTransactionData) => {
+          return {
+            node: TransactionWithXDRFactory.fromHorizon(record),
+            cursor: record.paging_token
+          };
+        }),
         pageInfo: {
           endCursor: records[records.length - 1].paging_token
         }
