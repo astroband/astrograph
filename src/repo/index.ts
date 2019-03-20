abbcb68a27971a41b39ce29ee098fb0c1d5b7251
Timur Ramazanov <charlie.vmk@gmail.com>
2019-03-20 18:36:31 +0300

    Fix repo/index.ts

--- src/repo/index.ts
+++ src/repo/index.ts
@@ -2,7 +2,6 @@ export * from "./accounts";
 export * from "./assets";
 export * from "./data_entries";
 export * from "./ledger_headers";
-export * from "./signers";
 export * from "./transactions";
 export * from "./trust_lines";
 export * from "./store_state";
