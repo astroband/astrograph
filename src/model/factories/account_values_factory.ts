476397f5b43f1b3eb6406d44212e518ffbba3e96
Timur Ramazanov <charlie.vmk@gmail.com>
2019-03-27 13:22:19 +0300

    Revert unnecessary changes

--- src/model/factories/account_values_factory.ts
+++ src/model/factories/account_values_factory.ts
@@ -2,20 +2,14 @@ import { AccountFlagsFactory } from "./account_flags_factory";
 import { AccountThresholdsFactory } from "./account_thresholds_factory";
 import { SignerFactory } from "./signer_factory";
 
-import { AccountValues, IAccountValues, Signer } from "../";
+import { AccountValues, IAccountValues } from "../";
 
 import { publicKeyFromXDR } from "../../util/xdr";
 
 export class AccountValuesFactory {
   public static fromXDR(xdr: any): AccountValues {
     const id = publicKeyFromXDR(xdr);
-    let signers: Signer[] = [];
-
-    // signers are not set for deleted accounts
-    if (xdr.signers) {
-      signers = xdr.signers().map((s: any) => SignerFactory.fromXDR(s, id));
-    }
-
+    const signers = xdr.signers().map((s: any) => SignerFactory.fromXDR(s, id));
     const thresholds = AccountThresholdsFactory.fromValue(xdr.thresholds());
 
     signers.unshift(SignerFactory.self(id, thresholds.masterWeight));
