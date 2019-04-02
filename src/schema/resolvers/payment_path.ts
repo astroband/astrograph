481ac7ebbf6242aa94430aec4148ee6ccbdae1e4
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-04-02 13:28:19 +0400

    Switched to standard resolvers

--- src/schema/resolvers/payment_path.ts
+++ src/schema/resolvers/payment_path.ts
@@ -1,16 +1,13 @@
-import { Asset } from "stellar-sdk";
 import { IHorizonPaymentPathData } from "../../datasource/types";
 import { AssetFactory } from "../../model/factories";
-
-const resolveAsset = (a: Asset): any => {
-  return {
-    native: a.isNative(),
-    code: a.getCode(),
-    issuer: a.getIssuer()
-  };
-};
+import { assetResolver } from "./util";
 
 export default {
+  PaymentPath: {
+    sourceAsset: assetResolver,
+    destinationAsset: assetResolver,
+    path: assetResolver
+  },
   Query: {
     async findPaymentPaths(root: any, args: any, ctx: any, info: any) {
       const { sourceAccountID, destinationAccountID, destinationAsset, destinationAmount } = args;
@@ -23,25 +20,21 @@ export default {
       );
 
       const r = records.map((record: IHorizonPaymentPathData) => {
-        const srcAsset = AssetFactory.fromHorizonResponse(
-          record.source_asset_type,
-          record.source_asset_code,
-          record.source_asset_issuer
-        );
-
-        const dstAsset = AssetFactory.fromHorizonResponse(
-          record.destination_asset_type,
-          record.destination_asset_code,
-          record.destination_asset_issuer
-        );
-
         const path = record.path.map((asset: any) =>
-          resolveAsset(AssetFactory.fromHorizonResponse(asset.asset_type, asset.asset_code, asset.asset_issuer))
+          AssetFactory.fromHorizonResponse(asset.asset_type, asset.asset_code, asset.asset_issuer)
         );
 
         return {
-          sourceAsset: resolveAsset(srcAsset),
-          destinationAsset: resolveAsset(dstAsset),
+          sourceAsset: AssetFactory.fromHorizonResponse(
+            record.source_asset_type,
+            record.source_asset_code,
+            record.source_asset_issuer
+          ),
+          destinationAsset: AssetFactory.fromHorizonResponse(
+            record.destination_asset_type,
+            record.destination_asset_code,
+            record.destination_asset_issuer
+          ),
           sourceAmount: record.source_amount,
           destinationAmount: record.destination_amount,
           path
