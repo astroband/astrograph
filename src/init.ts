4d8ec2732a3308fc71dda072fd67ab25a4766910
Sergey Nebolsin <sergey@nebols.in>
2019-04-21 04:22:40 +0300

    Upgrade deps, get rid of nodemon

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,4 @@
+import * as Integrations from "@sentry/integrations";
 import * as Sentry from "@sentry/node";
 import "./util/asset";
 import logger from "./util/logger";
@@ -9,7 +10,7 @@ export default function init(): void {
   if (SENTRY_DSN) {
     Sentry.init({
       dsn: SENTRY_DSN,
-      integrations: [new Sentry.Integrations.RewriteFrames({ root: __dirname || process.cwd() })]
+      integrations: [new Integrations.RewriteFrames({ root: __dirname || process.cwd() })]
     });
   }
 
