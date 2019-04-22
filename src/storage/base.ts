268efcd3d63ae06010a30a9893098904df83e07f
Timur Ramazanov <charlie.vmk@gmail.com>
2019-04-22 19:03:12 +0300

    Fixes for ES 7.0

--- src/storage/base.ts
+++ src/storage/base.ts
@@ -1,15 +1,6 @@
 import { Client as ElasticClient } from "@elastic/elasticsearch";
 import * as secrets from "../util/secrets";
 
-interface IElasticSearchHit {
-  _index: string;
-  _type: string;
-  _id: string;
-  _score: number;
-  _source: any; // TODO: here should be union of all data types we return from Elastic
-  _sort: Array<string | number>;
-}
-
 export abstract class BaseStorage {
   private client: ElasticClient;
 
@@ -19,25 +10,23 @@ export abstract class BaseStorage {
 
   protected abstract get elasticIndexName(): string;
 
-  protected async search(body: any) {
-    const { body: docs } = await this.client.search({
+  protected async search(requestBody: any) {
+    const { body: response } = await this.client.search({
       index: this.elasticIndexName,
-      body
+      body: requestBody
     });
 
-    return docs.hits.hits.map((h: IElasticSearchHit) => {
+    return response.hits.hits.map((h: any) => {
       h._source.paging_token = h._source.order.toString();
       return { ...h._source, id: h._id };
     });
   }
 
   protected async get(id: string) {
-    const { body: doc } = await this.client.get({
-      index: this.elasticIndexName,
-      type: "_doc",
-      id
-    });
-
-    return doc.found ? doc._source : null;
+    return this.client
+      .get({ index: this.elasticIndexName, id })
+      .then(({ body }: { body: any }) => {
+        return { ...body._source, id: body._id }
+      });
   }
 }
