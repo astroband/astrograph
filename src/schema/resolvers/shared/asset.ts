c7dfaff928c3c9386d71d2402fc34efee65f30cb
Timur Ramazanov <charlie.vmk@gmail.com>
2019-04-25 17:25:58 +0300

    Handle SDK assets in models too

--- src/schema/resolvers/shared/asset.ts
+++ src/schema/resolvers/shared/asset.ts
@@ -1,23 +1,28 @@
-import { Asset } from "stellar-sdk";
 import { db } from "../../../database";
 import { IApolloContext } from "../../../graphql_server";
-import { AssetID } from "../../../model";
+import { Asset, AssetID } from "../../../model";
 import { createBatchResolver, onlyFieldsRequested } from "../util";
 
 export const asset = createBatchResolver<any, Asset[]>((source: any, args: any, ctx: IApolloContext, info: any) => {
   const field = info.fieldName;
 
-  if (onlyFieldsRequested(info, ["code", "issuer"])) {
+  if (onlyFieldsRequested(info, ["code", "issuer", "native"])) {
     return source.map((obj: any) => {
-      if (obj[field] === "native") {
-        return { code: "XLM", issuer: null };
+      // this trick will return asset id either `obj[field]`
+      // is instance of SDK Asset class, either it's already a
+      // string asset id
+      const assetId = obj[field].toString();
+
+      if (assetId === "native") {
+        return { code: "XLM", issuer: null, native: true };
       }
-      const [code, issuer] = obj[field].split("-");
-      return { code, issuer };
+
+      const [code, issuer] = assetId.split("-");
+      return { code, issuer, native: false };
     });
   }
 
-  const ids: AssetID[] = source.map((s: any) => s[field]);
+  const ids: AssetID[] = source.map((s: any) => s[field].toString());
 
   return db.assets.findAllByIDs(ids);
 });
