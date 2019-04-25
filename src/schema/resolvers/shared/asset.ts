eb5e22bea3a958474fea06050b06beabcd810e6e
Timur Ramazanov <charlie.vmk@gmail.com>
2019-04-25 13:34:22 +0300

    Handle native lumens as regular asset

--- src/schema/resolvers/shared/asset.ts
+++ src/schema/resolvers/shared/asset.ts
@@ -1,17 +1,25 @@
 import { Asset } from "stellar-sdk";
+import { db } from "../../../database";
 import { IApolloContext } from "../../../graphql_server";
+import { AssetID } from "../../../model";
+import { createBatchResolver, onlyFieldsRequested } from "../util";
 
-export function asset(obj: any, args: any, ctx: IApolloContext, info: any) {
+export const asset = createBatchResolver<any, Asset[]>((source: any, args: any, ctx: IApolloContext, info: any) => {
   const field = info.fieldName;
-  const value = obj[field] as Asset;
 
-  const res = (a: Asset): any => {
-    return { code: a.getCode(), issuer: a.getIssuer(), native: a.isNative() };
-  };
-
-  if (Array.isArray(value)) {
-    return value.map(a => res(a));
+  if (onlyFieldsRequested(info, ["code", "issuer"])) {
+    return source.map((obj: any) => {
+      if (obj[field] === "native") {
+        return { code: "XLM", issuer: null };
+      }
+      const [code, issuer] = obj[field].split("-");
+      return { code, issuer };
+    });
   }
 
-  return res(value);
-}
+  const ids: AssetID[] = source.map((s: any) => s[field]);
+
+  console.log(ids);
+
+  return db.assets.findAllByIDs(ids);
+});
