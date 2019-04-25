319c990f20a3c8ded29fb89a81235081699c83f7
Timur Ramazanov <charlie.vmk@gmail.com>
2019-04-25 15:31:56 +0300

    List of assets from database

--- src/schema/resolvers/asset.ts
+++ src/schema/resolvers/asset.ts
@@ -1,14 +1,13 @@
 import { db } from "../../database";
-import { IHorizonAssetData } from "../../datasource/types";
 import { IApolloContext } from "../../graphql_server";
 import { Asset, AssetID } from "../../model";
 import { toFloatAmountString } from "../../util/stellar";
 import * as resolvers from "./shared";
 import { makeConnection } from "./util";
 
-const holdersResolver = async (root: any, args: any, ctx: IApolloContext, info: any) => {
-  const asset = root.native ? Asset.native() : new Asset(root.code, root.issuer);
-  const balances = await db.assets.findHolders(asset, args);
+const holdersResolver = async (root: Asset, args: any, ctx: IApolloContext, info: any) => {
+  const balances = await db.assets.findHolders(root.toInput(), args);
+
   return makeConnection(balances);
 };
 
@@ -16,7 +15,10 @@ export default {
   Asset: {
     issuer: resolvers.account,
     lastModifiedIn: resolvers.ledger,
-    totalSupply: (asset: Asset) => toFloatAmountString(asset.totalSupply),
+    totalSupply: (asset: Asset) => {
+      console.log(asset);
+      return toFloatAmountString(asset.totalSupply);
+    },
     circulatingSupply: (asset: Asset) => toFloatAmountString(asset.circulatingSupply),
     balances: holdersResolver
   },
@@ -26,25 +28,12 @@ export default {
     },
     assets: async (root: any, args: any, ctx: IApolloContext, info: any) => {
       const { code, issuer } = args;
-      const records: IHorizonAssetData[] = await ctx.dataSources.assets.all(
+      const records = await db.assets.findAll(
         code || issuer ? { code, issuer } : {},
         args
       );
 
-      return makeConnection(records, (r: IHorizonAssetData) => {
-        return {
-          code: r.asset_code,
-          issuer: r.asset_issuer,
-          native: r.asset_type === "native",
-          amount: r.amount,
-          numAccounts: r.num_accounts,
-          flags: {
-            authRequired: r.flags.auth_required,
-            authRevocable: r.flags.auth_revocable,
-            authImmutable: r.flags.auth_immutable
-          }
-        };
-      });
+      return makeConnection(records);
     }
   }
 };
