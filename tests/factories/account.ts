716e64d840ce246ac15484ab33b564528de64dd9
Timur Ramazanov <charlie.vmk@gmail.com>
2019-04-29 18:07:18 +0300

    Fix tests

--- tests/factories/account.ts
+++ tests/factories/account.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from "bignumber.js";
 import { Factory } from "rosie";
 import stellar from "stellar-base";
 import { Account } from "../../src/model";
@@ -11,7 +12,9 @@ Factory.define("account")
   .attr("homeDomain", "")
   .attr("thresholds", "AQAAAA==")
   .attr("flags", 0)
-  .attr("lastModified", 6);
+  .attr("lastModified", 6)
+  .attr("sellingLiabilities", new BigNumber("8927364"))
+  .attr("buyingLiabilities", new BigNumber("2948361"));
 
 export default {
   build(overrides?: object): Account {
