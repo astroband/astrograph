4049b4a36b262c9125e325e9d9f0944dd327ae41
Timur Ramazanov <charlie.vmk@gmail.com>
2019-06-06 15:36:21 +0300

    :cop:

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,5 @@
-import * as Sentry from "@sentry/node";
 import * as Integrations from "@sentry/integrations";
+import * as Sentry from "@sentry/node";
 import { createConnection, getRepository } from "typeorm";
 import { Account, AccountData, Offer, TrustLine } from "./orm/entities";
 import "./util/asset";
