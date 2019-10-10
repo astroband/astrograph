ca99a6d7149c85026e3a55a825da3140e887f33f
Timur Ramazanov <charlie.vmk@gmail.com>
2019-10-10 13:39:51 +0300

    Fix data entry subscription account resolving

--- src/model/data_entry_subscription_payload.ts
+++ src/model/data_entry_subscription_payload.ts
@@ -1,17 +1,17 @@
 import { publicKeyFromXDR } from "../util/xdr";
-import { DataEntryValues } from "./data_entry_values";
+import { AccountID, DataEntryValues } from "./";
 import { DataEntryValuesFactory } from "./factories/data_entry_values_factory";
 import { IMutationType, MutationType } from "./mutation_type";
 
 export class DataEntrySubscriptionPayload implements IMutationType {
-  public accountID: string;
+  public account: AccountID;
   public mutationType: MutationType;
   public name: string;
   public values: DataEntryValues | null = null;
 
   constructor(mutationType: MutationType, xdr: any) {
     this.mutationType = mutationType;
-    this.accountID = publicKeyFromXDR(xdr);
+    this.account = publicKeyFromXDR(xdr);
     this.name = xdr.dataName().toString();
 
     if (mutationType !== MutationType.Remove) {
