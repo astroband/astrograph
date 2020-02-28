0da6bea854207c53bbaa05ba9459f4e2fffe95b1
Timur Ramazanov <charlie.vmk@gmail.com>
2020-02-28 17:51:15 +0300

    Remove `console.log`

--- src/storage/connection.ts
+++ src/storage/connection.ts
@@ -2,6 +2,5 @@ import { Client as ElasticClient } from "@elastic/elasticsearch";
 import * as secrets from "../util/secrets";
 
 const connection = new ElasticClient({ node: secrets.ELASTIC_URL });
-console.log("Connected to elastic");
 
 export { connection }
