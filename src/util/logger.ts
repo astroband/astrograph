4d70428574d3c115332a661f2e86efba3834ace1
Sergey Nebolsin <nebolsin@gmail.com>
2020-06-06 22:29:50 +0300

    Fix the build

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,5 +1,7 @@
 import { createLogger, format, Logger, transports } from "winston";
 
+const { colorize, combine, errors, prettyPrint, splat, timestamp } = format;
+
 enum LoggerLevel {
   error = "error",
   warn = "warn",
@@ -17,22 +19,14 @@ let logLevel = process.env.LOG_LEVEL;
 if (!logLevel || !LoggerLevel[logLevel]) {
   logLevel = process.env.NODE_ENV === "production" ? PROD_LOG_LEVEL : DEV_LOG_LEVEL;
 }
-const errorWithStacktraceFormat = format(info => {
-  if (info instanceof Error) {
-    return { message: info.message, stack: info.stack, ...info };
-  }
-
-  return info;
-});
 
 const logger: Logger = createLogger({
-  format: format.combine(
-    errorWithStacktraceFormat(),
-    format.colorize(),
-    format.splat(),
-    format.timestamp(),
-    format.simple()
-    // format.printf(info => `${info.timestamp} ${info.level}: ${info.message} Stacktrace: ${info.stack}`)
+  format: combine(
+    errors({ stack: true }),
+    colorize(),
+    splat(),
+    timestamp(),
+    prettyPrint(),
   ),
   transports: [
     new transports.Console({ level: logLevel }),
